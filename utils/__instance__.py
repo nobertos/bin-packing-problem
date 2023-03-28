7fb38a1180c433e78933253a134e5c10589ea519
Rayene Nassim ZORGANI <jr_zorgani@esi.dz>
2023-03-28 15:51:21 +0100

    Edit: removed useless code from __instance__.py

--- utils/__instance__.py
+++ utils/__instance__.py
@@ -5,7 +5,5 @@ with open("utils/__instance__.txt", "r") as f:
     MAX_CAPACITY = int(lines[1].strip())
     INPUT_ITEMS: List[int] = [int(line.strip()) for line in lines[2:]]
 
-MAX_CAPACITY = 70
-INPUT_ITEMS = [44, 6, 24, 6, 24, 8, 22, 8, 17, 21]
-NUM_ITEMS = len(INPUT_ITEMS)
+
 print(f"len(INPUT_ITEMS): {NUM_ITEMS}")
